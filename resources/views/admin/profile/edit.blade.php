fc2b29342c47d04eaed7ef44a5a1eeced4280694
zeelorenc <zeelorenc@hotmail.com>
2021-08-10 21:14:26 +1000

    test branch rename

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -47,7 +47,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-12">
                                     <button type="submit" class="btn btn-block btn-primary">
-                                        {{ __('Save Change') }}
+                                        {{ __('Save Changes') }}
                                     </button>
                                 </div>
                             </div>
