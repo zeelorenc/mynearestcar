667d6520486b242e9885e25959736a276fcef9f9
Ziming Wu <s3678134@student.rmit.edu.au>
2021-08-12 23:35:39 +1000

    PBI-57 done admin side layout design and implement

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="footer-left">
+    Copyright &copy; {{ date('Y') }}
+    <div class="bullet"></div>
+    Developed by <a href="//github.com/rmit-s3660109-lorenc-pekaj/PP-1-S2-2021-WED0430PM-Group1">PP-1-S2-2021-WED0430PM-Group1</a>
+</div>
+<div class="footer-right"></div>
