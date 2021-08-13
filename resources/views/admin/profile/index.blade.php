945ced5c91f644d782ac63d547e5223feb4de3cc
Lorenc Pekaj s3660109 <s3660109@student.rmit.edu.au>
2021-08-14 01:01:20 +1000

    PBI-51 clean up

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -4,7 +4,9 @@
 
     <section class="section">
         <div class="section-header">
-            <h1>{{ __('My Profile') }}</h1>
+            <h1>
+                {{ $currentUser->id === $user->id ? 'My Profile' : "Profile of {$user->name}" }}
+            </h1>
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="#">{{ __('Dashboard') }}</a></div>
                 <div class="breadcrumb-item">{{ __('Profile') }}</div>
@@ -12,21 +14,25 @@
         </div>
 
         <div class="section-body">
-            <h2 class="section-title">Hi, {{ $user->name }}!</h2>
-            <p class="section-lead">
-                Change information about yourself on this page.
-            </p>
+            @if ($currentUser->id === $user->id)
+                <h2 class="section-title">Hi, {{ $user->name }}!</h2>
+                <p class="section-lead">
+                    Change information about yourself on this page.
+                </p>
+            @endif
 
+            @if(session()->has('message'))
+                <div class="alert alert-success mb-2">{{ session()->get('message') }}</div>
+            @endif
 
             <div class="row mt-sm-4">
-                <div class="col-12 col-lg-5">
-                    <div class="card profile-widget">
+                <div class="col-12 col-lg-4">
+                    <div class="card profile-widget mt-0">
                         <div class="profile-widget-header">
-                            <img alt="image" src="{{ asset('assets/img/avatar/avatar-4.png') }}" class="rounded-circle profile-widget-picture">
                             <div class="profile-widget-items">
                                 <div class="profile-widget-item">
                                     <div class="profile-widget-item-label">Role</div>
-                                    <div class="profile-widget-item-value">{{ $user->role }}</div>
+                                    <div class="profile-widget-item-value">{{ ucfirst($user->role) }}</div>
                                 </div>
                             </div>
                         </div>
@@ -38,10 +44,6 @@
                                     <b>{{ $user->name }}</b>
                                 </li>
                                 <li class="list-group-item d-flex justify-content-between align-items-center">
-                                    <span>Phone Number:</span>
-                                    <b></b>
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center">
                                     <span>Email:</span>
                                     <b>{{ $user->email }}</b>
                                 </li>
@@ -50,7 +52,7 @@
                     </div>
                 </div>
 
-                <div class="col-12 col-md-12 col-lg-7">
+                <div class="col-12 col-md-12 col-lg-8">
                     <div class="card">
                         <form method="POST" action="{{ route('admin.profile.update', $user->id) }}">
                             @csrf
@@ -61,14 +63,12 @@
                             </div>
 
                             <div class="card-body">
-                                @if(session()->has('message'))
-                                    <div class="alert alert-success mb-2">{{ session()->get('message') }}</div>
-                                @endif
-
                                 <div class="row">
                                     <div class="form-group col-12">
                                         <label>Name</label>
-                                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $user->name }}" required autocomplete="name" autofocus>
+                                        <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                               name="name" value="{{ $user->name }}" required autocomplete="name"
+                                               autofocus>
                                         @error('name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -78,7 +78,8 @@
 
                                     <div class="form-group col-12">
                                         <label>Email</label>
-                                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="{{ $user->email }}" autocomplete="email">
+                                        <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                               name="email" placeholder="{{ $user->email }}" autocomplete="email">
                                         @error('email')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -100,18 +101,16 @@
                                 <h4>Change Password</h4>
                             </div>
                             <div class="card-body">
-                                @if(session()->has('message'))
-                                    <div class="alert alert-success mb-2">{{ session()->get('message') }}</div>
-                                @endif
-
                                 <div class="row">
                                     <div class="form-group col-md-6 col-12">
                                         <label for="pwd">Password *</label>
-                                        <input type="password" id="pwd" class="form-control" name="pwd[password]" required="">
+                                        <input type="password" id="pwd" class="form-control" name="pwd[password]"
+                                               required="">
                                     </div>
                                     <div class="form-group col-md-6 col-12">
                                         <label for="repwd">Confirm Password *</label>
-                                        <input type="password" id="repwd" class="form-control" name="pwd[repwd]" required="">
+                                        <input type="password" id="repwd" class="form-control" name="pwd[repwd]"
+                                               required="">
                                     </div>
                                 </div>
                             </div>
