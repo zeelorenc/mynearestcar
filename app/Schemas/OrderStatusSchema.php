b923fafaf9dab601e1f378b54d02ae9e827f3b65
Lorenc Pekaj s3660109 <s3660109@student.rmit.edu.au>
2021-08-25 00:02:27 +1000

    PBI-14 / PBI-18 add stripe payment support and rental order payment

--- app/Schemas/OrderStatusSchema.php
+++ app/Schemas/OrderStatusSchema.php
@@ -6,4 +6,16 @@ class OrderStatusSchema
 {
     public const UNPAID = 'unpaid';
     public const PAID = 'paid';
+
+    /**
+     * @param string $status
+     * @return string
+     */
+    public static function asCssClass(string $status): string
+    {
+        return [
+            self::PAID => 'text-success',
+            self::UNPAID => 'text-danger',
+        ][$status] ?? '';
+    }
 }
