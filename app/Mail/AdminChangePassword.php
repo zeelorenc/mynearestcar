5a52818d606eacdecb0416d8e1636cc92313e886
Ziming Wu <s3678134@student.rmit.edu.au>
2021-08-27 00:17:19 +1000

    Added admin change password

---
+++ app/Mail/AdminChangePassword.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class AdminChangePassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->to($this->user->email)
+            ->subject('Password has been changed')
+            ->view('emails.admin_change_password');
+    }
+}
+
