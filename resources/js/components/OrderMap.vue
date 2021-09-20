d632a6307d9cab8a0df13df9ac175caad7f2f297
Lorenc Pekaj s3660109 <s3660109@student.rmit.edu.au>
2021-09-21 01:14:45 +1000

    PBI-19 add directions of uber into order rental modal

---
+++ resources/js/components/OrderMap.vue
@@ -0,0 +1,92 @@
+<template>
+
+    <div class="row" v-show="visible">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body p-0 overflow-hidden rounded">
+                    <div class="google-map-container map-modal">
+                        <GmapMap
+                            ref='uberMapRef'
+                            :zoom='12'
+                            map-type-id='terrain'
+                            :center="{ lat: -37.8136, lng: 144.9631 }"
+                            :options='{
+                                mapTypeControl: false,
+                                streetViewControl: false,
+                                fullscreenControl: false,
+                                zoomControl: false,
+                            }'
+                        >
+                            <!-- user location -->
+                            <UserMarker />
+
+                            <!-- carpark location -->
+                            <CarparkMarker :carpark="carpark"/>
+                        </GmapMap>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['vehicle', 'carpark', 'visible'],
+
+    data() {
+        return {
+            directionsRender: null,
+            uberMapRef: null,
+            directionsResponse: null,
+        };
+    },
+
+    watch: {
+        visible: function () {
+            if (this.visible === true && this.directionsResponse !== null) {
+                this.directionsRender.setDirections(this.directionsResponse);
+            }
+        }
+    },
+
+    mounted() {
+        this.$refs.uberMapRef.$mapPromise.then(() => {
+            console.log('this.$root.currentLocation', this.$root.currentLocation);
+            console.log('carpark', {'lat': this.carpark.lat, 'lng': this.carpark.lng});
+
+            this.setDirections(
+                this.$root.currentLocation,
+                {'lat': this.carpark.lat, 'lng': this.carpark.lng}
+            );
+        });
+    },
+
+    methods: {
+        setDirections: function (start, finish) {
+            const mapObject = this.$refs.uberMapRef.$mapObject;
+            if (this.directionsRender !== null) {
+                this.directionsRender.setMap(null);
+            }
+            this.directionsRender = new google.maps.DirectionsRenderer({suppressMarkers: true});
+            this.directionsRender.setMap(mapObject);
+            new google.maps.DirectionsService().route(
+                {
+                    origin: start,
+                    destination: finish,
+                    optimizeWaypoints: true,
+                    travelMode: 'DRIVING'
+                },
+                (response, status) => {
+                    if (status === 'OK') {
+                        this.directionsResponse = response;
+                        this.directionsRender.setDirections(response);
+                        const distance = response.routes[0].legs[0].distance.value;
+                        this.$emit('calculated', distance);
+                    }
+                }
+            );
+        }
+    },
+}
+</script>
