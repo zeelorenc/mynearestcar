5df87e31af4587cfa2dd3a12eb63030d5d7f585d
zeelorenc <zeelorenc@hotmail.com>
2021-09-22 00:49:42 +1000

    PBI-59 customer homepage

--- resources/views/rent.blade.php
+++ resources/views/rent.blade.php
@@ -4,9 +4,10 @@
 
     <section class="section">
         <div class="section-header">
-            <h1>Welcome back, {{ $currentUser->name }}</h1>
+            <h1>Rent a Car</h1>
         </div>
         <div class="section-body">
+            <map-component></map-component>
         </div>
     </section>
 
