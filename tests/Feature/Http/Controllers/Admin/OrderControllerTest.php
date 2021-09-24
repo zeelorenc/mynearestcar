574c1b7584a36415a61876cab740e4bd07d76137
zeelorenc <zeelorenc@hotmail.com>
2021-09-25 01:34:20 +1000

    PBI-26 order search

--- tests/Feature/Http/Controllers/Admin/OrderControllerTest.php
+++ tests/Feature/Http/Controllers/Admin/OrderControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers\Admin;
 
+use App\Models\Carpark;
 use App\Models\Order;
 use App\Models\User;
 use App\Models\Vehicle;
@@ -15,6 +16,12 @@ class OrderControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->actingAsAdmin();
+    }
+
     /**
      * @test
      *
@@ -22,7 +29,6 @@ class OrderControllerTest extends TestCase
      */
     public function it_can_search_for_a_vehicle(): void
     {
-        $this->actingAsAdmin();
         $order = Order::factory()->create();
 
         $response = $this->post(route('admin.order.search'), [
@@ -44,7 +50,6 @@ class OrderControllerTest extends TestCase
      */
     public function it_can_mark_a_returned_vehicle_as_available(): void
     {
-        $this->actingAsAdmin();
         $vehicle = Vehicle::factory()->create(['status' => VehicleStatusSchema::RETURNED]);
         $order = Order::factory()->for($vehicle)->create(['status' => OrderStatusSchema::PAID]);
 
@@ -53,6 +58,38 @@ class OrderControllerTest extends TestCase
 
         $this->assertEquals(OrderStatusSchema::COMPLETED, $order->status);
         $this->assertEquals(VehicleStatusSchema::AVAILABLE, $order->vehicle->status);
+    }
+
+    /** @test */
+    public function it_can_search_given_vehicle_or_user_names(): void
+    {
+        $userOne = User::factory()->create(['name' => 'John']);
+        $userTwo = User::factory()->create(['name' => 'Mary']);
+
+        $vehicleOne = Vehicle::factory()->create(['name' => 'Tesla Model A']);
+        $vehicleTwo = Vehicle::factory()->create(['name' => 'Mercedes AMG GT']);
+        $vehicleThree = Vehicle::factory()->create(['name' => 'Ferrari 458']);
+
+        Order::factory()->create(['user_id' => $userTwo, 'vehicle_id' => $vehicleOne]);
+        Order::factory()->create(['user_id' => $userOne, 'vehicle_id' => $vehicleTwo]);
+        Order::factory()->create(['user_id' => $userOne, 'vehicle_id' => $vehicleThree]);
+
+        $this->get(route('admin.order.search', ['query' => 'John']))
+            ->assertSee(['Mercedes', 'Ferrari'])
+            ->assertDontSee('Tesla');
+
+        $this->get(route('admin.order.search', ['query' => 'Mary']))
+            ->assertSee('Tesla')
+            ->assertDontSee(['Mercedes', 'Ferrari']);
+
+        $this->get(route('admin.order.search', ['query' => 'Ferrari']))
+            ->assertSee('John');
+
+        $this->get(route('admin.order.search', ['query' => 'Mercedes']))
+            ->assertSee('John');
+
+        $this->get(route('admin.order.search', ['query' => 'Tesla']))
+            ->assertSee('Mary');
     }
 
     /**
