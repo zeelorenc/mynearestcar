495e4e8678441a8531382a69fd42d824771f2dba
Lorenc Pekaj s3660109 <s3660109@student.rmit.edu.au>
2021-09-25 01:59:59 +1000

    PBI-23 add test

--- tests/Feature/Http/Controllers/OrderControllerTest.php
+++ tests/Feature/Http/Controllers/OrderControllerTest.php
@@ -2,10 +2,19 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class OrderControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->actingAsUser();
+    }
+
     /**
      * @test
      *
@@ -15,5 +24,16 @@ class OrderControllerTest extends TestCase
     {
         // @todo create mock order then see if it loads page
         $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function it_can_render_the_history_page(): void
+    {
+        $this->get(route('order.history'))
+            ->assertViewIs('order.history');
     }
 }
