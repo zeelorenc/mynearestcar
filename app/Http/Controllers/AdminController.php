a5c950d12cf9699651f77a1db0bf5d2f1571aa0a
Zeyi Wu <s3798349@student.rmit.edu.au>
2021-10-09 17:02:02 +0800

    added displaying delayed orders

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,16 +6,31 @@ use App\Http\Controllers\Controller;
 use App\Models\Carpark;
 use App\Models\User;
 use App\Models\Vehicle;
+use App\Models\Order;
+use App\Schemas\OrderStatusSchema;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {
+        $orders = Order::query()
+        ->where('to_date', '<', date('Y-m-d', time()))
+        ->Where(function($query) {
+            $query->where('status', OrderStatusSchema::PAID)
+                  ->orWhere('status', OrderStatusSchema::UNPAID);
+        });
+
+
+        $orders = $orders
+            ->orderBy('created_at', 'desc')
+            ->paginate(5);
+
         return view('admin.home')
             ->with('totalUsers', User::all()->count())
             ->with('totalCarparks', Carpark::all()->count())
-            ->with('totalVehicles', Vehicle::all()->count());
+            ->with('totalVehicles', Vehicle::all()->count())
+            ->with('delayedOrders', $orders);
     }
 
     public function login()
