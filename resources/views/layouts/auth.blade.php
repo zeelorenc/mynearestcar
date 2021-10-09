07bf69618348fcc550bd9f98907a1ecce7109e9c
zeelorenc <zeelorenc@hotmail.com>
2021-10-09 23:59:02 +1100

    PBI-8 update title

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Login &mdash; Stisla</title>
+    <title>Login &mdash; {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Template CSS -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
