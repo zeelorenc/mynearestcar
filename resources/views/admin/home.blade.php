29b2bf52e86b109f2fcc5f9e0682ba4a537ad687
zeelorenc <zeelorenc@hotmail.com>
2021-10-09 23:18:57 +1100

    PBI-21 cleanup

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -74,55 +74,59 @@
                 </div>
             </div>
 
-            @if (count($delayedOrders))
             <div class="card">
                 <div class="card-header">
                     <h4>Overdue Rentals</h4>
                     <div class="card-header-action">
-                        <a href="#" class="btn btn-primary">View All</a>
+                        <a href="{{ route('admin.order.search') }}" class="btn btn-primary">Search Orders</a>
                     </div>
                 </div>
                 <div class="card-body p-0">
                     <div class="table-responsive">
-                        <table class="table table-bordered table-md">
-                            <tbody><tr>
-                                <th>Order ID</th>
-                                <th>User</th>
-                                <th>Vehicle</th>
-                                <th>Rental Started</th>
-                                <th>Rental Due</th>
-                                <th>Uber pickup</th>
-                                <th>Total Price</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                            @foreach ($delayedOrders as $order)
-                            <tr>
-                                <td>{{ $order->id }}</td>
-                                <td>{{ $order->user->name }}</td>
-                                <td>{{ $order->vehicle->name }}</td>
-                                <td>{{ $order->from_date->toFormattedDateString() }}</td>
-                                <td>{{ $order->to_date->toFormattedDateString() }}</td>
-                                <td>{{ $order->uber_pickup ? 'Yes' : 'No' }}</td>
-                                <td>${{ $order->grand_total }}</td>
-                                <td>{{ ucfirst($order->status) }}</td>
-                                <td>
-                                    <form action="{{ route('admin.order.forceUpdate', $order->id) }}" method="POST">
-                                        @csrf
-                                        {{ method_field('put') }}
-                                        <button
-                                            class="btn btn-primary btn-block"
-                                            @if ($order->status == \App\Schemas\OrderStatusSchema::UNPAID)
-                                                disabled="disabled"
-                                            @endif
-                                        >
-                                        Confirm Return
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                            </tbody></table>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Order ID</th>
+                                    <th>User</th>
+                                    <th>Vehicle</th>
+                                    <th>Rental Started</th>
+                                    <th>Rental Due</th>
+                                    <th>Uber pickup</th>
+                                    <th>Total Price</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse($delayedOrders as $order)
+                                    <tr>
+                                        <td>{{ $order->id }}</td>
+                                        <td>{{ $order->user->name }}</td>
+                                        <td>{{ $order->vehicle->name }}</td>
+                                        <td>{{ $order->from_date->toFormattedDateString() }}</td>
+                                        <td>{{ $order->to_date->toFormattedDateString() }}</td>
+                                        <td>{{ $order->uber_pickup ? 'Yes' : 'No' }}</td>
+                                        <td>${{ $order->grand_total }}</td>
+                                        <td>{{ ucfirst($order->status) }}</td>
+                                        <td>
+                                            <form action="{{ route('admin.order.complete', $order->id) }}" method="POST">
+                                                @csrf
+                                                {{ method_field('put') }}
+                                                <button
+                                                    class="btn btn-primary btn-block"
+                                                >
+                                                    Force Complete
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="9" class="text-center">Nothing overdue yet</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
                     </div>
                 </div>
                 <div class="card-footer text-right">
@@ -131,7 +135,6 @@
                     </nav>
                 </div>
             </div>
-            @endif
         </div>
     </section>
 
